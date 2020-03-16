b9601b55fa1d9e8500e808f68e31b5bc7ee1688d
ilyaryabikin <ilyaryabikin0708@gmail.com>
2020-03-16 22:58:13 +0300

    Updated html formatting of HtmlExporter

--- TheBankStatementAnalyzer/src/main/java/bankstatement/exporter/HtmlExporter.java
+++ TheBankStatementAnalyzer/src/main/java/bankstatement/exporter/HtmlExporter.java
@@ -6,16 +6,16 @@ public class HtmlExporter implements Exporter {
 
     @Override
     public String export(SummaryStatistics summaryStatistics) {
-        return "<!doctype html>" + "<html lang='en'>" +
-                "<head><title>Bank Transaction Report</title></head>" +
-                "<body>" +
-                "<ul>" +
-                "<li><strong>The sum is</strong>: " + summaryStatistics.getSum() + "</li>" +
-                "<li><strong>The average is</strong>: " + summaryStatistics.getAverage() + "</li>" +
-                "<li><strong>The max is</strong>: " + summaryStatistics.getMax() + "</li>" +
-                "<li><strong>The min is</strong>: " + summaryStatistics.getMin() + "</li>" +
-                "</ul>" +
-                "</body>" +
+        return "<!doctype html>\n" + "<html lang='en'>\n" +
+                "<head><title>Bank Transaction Report</title></head>\n" +
+                "<body>\n" +
+                "   <ul>\n" +
+                "       <li><strong>The sum is</strong>: " + summaryStatistics.getSum() + "</li>\n" +
+                "       <li><strong>The average is</strong>: " + summaryStatistics.getAverage() + "</li>\n" +
+                "       <li><strong>The max is</strong>: " + summaryStatistics.getMax() + "</li>\n" +
+                "       <li><strong>The min is</strong>: " + summaryStatistics.getMin() + "</li>\n" +
+                "   </ul>\n" +
+                "</body>\n" +
                 "</html>";
     }
 }
